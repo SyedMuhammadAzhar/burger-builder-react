ad3f424740aa7cff45279395e24ca9c8ca31dd0f
SyedAzhar <syed.moh.azhar.98@gmail.com>
2021-01-08 15:48:10 +0500

    updating burger prices

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -15,6 +15,8 @@ const controls=[
 const buildControls=(props)=>(
     <div className={classes.BuildControls}>
 
+        <p> Current Price:<strong>{props.price.toFixed(2)}</strong></p>
+
        {controls.map(cntrl=>{
            return(<BuildControl
              key={cntrl.Label} 
